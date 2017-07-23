a8ddb88c7dde5d7ef62149eb5d8cae3087150380
Ondrej Mladek <ondrej.mladek@actum.cz>
2017-07-23 21:57:09 +0200

    + refactoring

--- src/app/components/form-validation.js
+++ src/app/components/form-validation.js
@@ -3,11 +3,18 @@
  *
  * email: /.+\@.+\..+/
  * number: /^\d+$/
+ * not empty: /^\s*\S.*$/
+ *
+ * data attributes:
+ * data-validation-format=".+\@.+\..+"
+ * data-validation-message-format-invalid="Incorrect email format"
+ * data-validation-required
+ * data-validation-message-required="Filed password is required"
  */
 
 const formValidation = (container) => {
     const form = container;
-    const inputsToValidate = Array.from(form.querySelectorAll('[data-validation-format]'));
+    const inputsToValidate = Array.from(form.querySelectorAll('[data-validation-format], [data-validation-required]'));
 
     /* Utils */
     const hasSuccessClassName = 'has-success';
@@ -20,14 +27,14 @@ const formValidation = (container) => {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        validateInputs(inputsToValidate);
+        validateAll(inputsToValidate);
 
         formIsValid && sendData();
     });
 
-    /* Listen for submit */
+    /* Listen for blur */
     inputsToValidate.forEach((input) => {
-        input.addEventListener('blur', event => validateInput(input));
+        input.addEventListener('blur', event => validate(input));
     });
 
     /**
@@ -39,46 +46,110 @@ const formValidation = (container) => {
         return true;
     }
 
-    function validateInputs(inputs) {
-        inputs.forEach(input => validateInput(input));
+    function validateAll(inputs) {
+        inputs.forEach(input => validate(input));
     }
 
     /**
      * @param  {HTML element} input
      */
-    function validateInput(input) {
+    function validate(input) {
         formIsValid = true;
-        const formatIsValid = isFormatValid(input);
-        const parentElement = input.parentElement;
-        const feedback = parentElement.querySelector('.form-control-feedback');
-        const formatInvalidMessage = input.dataset.validationMessageFormatInvalid;
+        const validation = getValidationRuleAndMessage(input);
+        const inputIsValid = isValid(input, validation.rule);
 
-        if (formatIsValid) {
-            parentElement.classList.add(hasSuccessClassName);
-            parentElement.classList.remove(hasDangerClassName);
-            feedback.innerText = '';
+        /* Show/hide validation message */
+        if (inputIsValid) {
+            handleValid(input);
         } else {
-            parentElement.classList.remove(hasSuccessClassName);
-            parentElement.classList.add(hasDangerClassName);
-            feedback.innerText = formatInvalidMessage;
+            handleInvalid(input, validation.message);
         }
 
-        if (!formatIsValid) {
+        /* Update form state */
+        if (!inputIsValid) {
             formIsValid = false;
         }
+    }
+
+    /**
+     * @param  {HTML element} input
+     * @return {object}
+     */
+    function getValidationRuleAndMessage(input) {
+        let rule;
+        let message;
+
+        /* TODO: rewrite without disabling eslint rule */
+        /* eslint-disable consistent-return */
+        /* Get value format rule (regular expression taken from data-attribute */
+        if (input.dataset.validationFormat) {
+            rule = new RegExp(input.dataset.validationFormat);
+            message = input.dataset.validationMessageFormatInvalid;
+        }
+
+        /* Check if input is required without explicit rule of value format */
+        if (input.dataset.validationRequired) {
+            switch (input.type) {
+                case 'text':
+                case 'number':
+                case 'password':
+                case 'textarea':
+                    rule = /^\s*\S.*$/;
+                    break;
+                case 'checkbox':
+                    rule = /^\s*\S.*$/;
+                    break;
+                case 'radio':
+                    rule = /^\s*\S.*$/;
+                    break;
+                default:
+                    return;
+            }
+
+            message = input.dataset.validationMessageRequired;
+        }
+
+        return {
+            rule,
+            message
+        };
+        /* eslint-enable consistent-return */
+    }
+
+    /**
+     * @param  {HTML element} input
+     */
+    function handleValid(input) {
+        const parentElement = input.parentElement;
+        const feedback = parentElement.querySelector('.form-control-feedback');
+
+        parentElement.classList.add(hasSuccessClassName);
+        parentElement.classList.remove(hasDangerClassName);
+        feedback.innerText = '';
+    }
+
+    /**
+     * @param  {HTML element} input
+     * @param  {string} message
+     */
+    function handleInvalid(input, message) {
+        const parentElement = input.parentElement;
+        const feedback = parentElement.querySelector('.form-control-feedback');
+
+        parentElement.classList.remove(hasSuccessClassName);
+        parentElement.classList.add(hasDangerClassName);
+        feedback.innerText = message;
     }
 
     /**
      * Test input value format. (ie. email)
      *
      * @param  {HTML element} input
+     * @param  {regular expression} rule
      * @return {bool}
      */
-    function isFormatValid(input) {
-        const format = new RegExp(input.dataset.validationFormat);
-        const value = input.value;
-
-        return format.test(value);
+    function isValid(input, rule) {
+        return rule.test(input.value);
     }
 };
 
